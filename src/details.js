72b70c2036fd55a2696c909e8381d13a572f7abc
kimdoye3 <doyeonkim.2012@gmail.com>
2023-06-26 22:31:26 -0400

    updated alert

--- src/details.js
+++ src/details.js
@@ -4,16 +4,6 @@ import { useParams } from "react-router-dom";
 function Detail(props) {
 
     let [alert, setAlert] = useState(true);
-    let [inputText, setInputText] = useState("");
-    let [amountAlert, setAmountAlert] = useState(false);
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setInputText(e.target.value);
-        if (!Number.isInteger(+inputText)){
-            setAmountAlert(true);
-            let timer = setTimeout(()=>{setAmountAlert(false)}, 5000)
-        }
-      };
 
     useEffect(()=>{
         // use for complicated calculation
@@ -51,10 +41,7 @@ function Detail(props) {
                 <h4 className="pt-5"> {props.products[id].title} </h4>
                 <p>{props.products[id].content}</p>
                 <p>${props.products[id].price}</p>
-                {amountAlert && <div id= 'alertAmount' className="alert alert-warning"> 
-                Type only number!
-                </div>}
-                <input name="amount" onChange={handleChange} value={inputText}/>
+                <Alert/>
                 <div className="col-md-6"></div>
                 <button className="btn btn-danger">주문하기</button> 
                 </div>
@@ -63,6 +50,21 @@ function Detail(props) {
     )
 
     
+}
+
+function Alert(){
+    
+    let [num, setNum] = useState('')
+
+    useEffect(()=>{
+      if (isNaN(num) == true){
+        alert('Please only enter numbers')
+      }
+    }, [num])
+  
+    return (
+      <input onChange={(e)=>{ setNum(e.target.value) }} />
+    )
 }
 
 export default Detail;
